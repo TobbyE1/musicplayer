6be77fe6463c7dfb260ce9268fbc6eebda8cd5ff
Tobi Emmanuel <cyber-one@mcnz.com>
2024-10-01 18:47:51 +0100

    img,msc

---
+++ Data.js
@@ -0,0 +1,32 @@
+let allmusic = [
+    {
+        name: "Deeper In Love",//Music name
+        artist: "Don Moen",//Artist name
+        img: "Images/image1",//Image path
+        src: "Musics/music1"//Music path
+    },
+    {
+        name: "Goodness of God",
+        artist: "cece winnas",
+        img: "Images/image2",
+        src: "Musics/music2"
+    },
+    {
+        name: "Praise",
+        artist: "Elevation Worship",
+        img: "Images/image3",
+        src: "Musics/music3"
+    },
+    {
+        name: "Season Of hope",
+        artist: "Don Moen",
+        img: "Images/image4",
+        src: "Musics/music4"
+    },
+    {
+        name: "Walk By Faith",
+        artist: "Aaron williams",
+        img: "Images/image2",
+        src: "Musics/music5"
+    }
+]
